21a60845d056a971b9b1afb0e0f3dd6095663949
Hanif D <hanifdawjee@gmail.com>
2020-02-12 19:35:55 +0000

    home mostly finished

--- index.php
+++ index.php
@@ -3,10 +3,54 @@
 <main>
 	<div class="container">
 		<div class="left">
-			
+	       <div class="inner-bar">
+	          	<div class="block active">
+					<div class="inner">
+				        <p>From</p>
+				        <?php echo file_get_contents("app/img/icons/icon-1.svg"); ?>
+					</div>
+	          	</div>
+	          	<div class="block">
+					<div class="inner">
+						<p>To</p>
+        				<?php echo file_get_contents("app/img/icons/icon-2.svg"); ?>
+					</div>
+	          	</div>
+	          	<div class="block">
+					<div class="inner">
+						<p>When</p>
+    					<?php echo file_get_contents("app/img/icons/icon-3.svg"); ?>
+					</div>
+	          	</div>
+	          	<div class="block">
+					<div class="inner">
+						<p>Who</p>
+        				<?php echo file_get_contents("app/img/icons/icon-4.svg"); ?>
+					</div>
+	          	</div>
+	          	<div class="block extra">
+					<div class="inner">
+						<p>Extras</p>
+        				<?php echo file_get_contents("app/img/icons/icon-5.svg"); ?>
+					</div>
+	          	</div>
+	          	<div class="block">
+					<div class="inner">
+						<p>Your Ticket</p>
+	    				<?php echo file_get_contents("app/img/icons/icon-6.svg"); ?>
+					</div>
+	          	</div>
+	        </div>
 		</div>
 		<div class="right">
-			
+	        <div class="content">
+	          <p>START YOUR JOURNEY</p>
+	          <h1>Where would<br>you like to go?</h1>
+	          <form action="#" id="search">
+	            <input type="text" placeholder='Station or location'>
+	            <input type="submit" value="submit">
+	          </form>
+	        </div>
 		</div>
 	</div>
 </main>
