5db38861b044951918b7b03e3e43f428c3ce3441
Hanif D <hanifdawjee@gmail.com>
2020-02-12 19:00:33 +0000

    header markup

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php require_once('partials/header.php'); ?>
 
+<main>
+	<div class="container">
+		<div class="left">
+			
+		</div>
+		<div class="right">
+			
+		</div>
+	</div>
+</main>
+
 <?php require_once('partials/footer.php'); ?>
--- partials/header.php
+++ partials/header.php
@@ -7,3 +7,30 @@
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300&display=swap" rel="stylesheet">
 </head>
 <body>
+  <header>
+    <div class="container">
+      <div class="left">
+          <img src="app/img/logo.jpg" alt="" width='200'>
+      </div>
+      <div class="right">
+        <div class="content">
+          <ul>
+            <li>Unbeatable prices. Guaranteed</li>
+            <li>No booking fees. No card fees</li>
+            <li>Cheap train tickets. Faster</li>
+          </ul>
+        </div>
+        <div class="icons">
+          <ul>
+            <li><img src="app/img/icons/search.png" alt=""></li>
+            <li><img src="app/img/icons/user.png" alt=""></li>
+          </ul>
+        <div id="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        </div>
+      </div>
+    </div>
+  </header>
